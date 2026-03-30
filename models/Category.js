8f1cdb055df93711c5002540e9e557583bd9d9e0
samtronicssolutions-source <samtronics.solutions@gmail.com>
2026-03-30 15:35:02 -0700

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -4,7 +4,8 @@ const categorySchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    trim: true
   },
   parent_id: {
     type: mongoose.Schema.Types.ObjectId,
